67db8eff357df693490602957e5db9a8127368a8
lightyears <915584360@qq.com>
2022-08-10 19:53:29 +0800

    framework

--- src/util/frontend/SysYParser.java
+++ src/util/frontend/SysYParser.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 
 @SuppressWarnings({"all", "warnings", "unchecked", "unused", "cast"})
 public class SysYParser extends Parser {
-	static { RuntimeMetaData.checkVersion("4.10.1", RuntimeMetaData.VERSION); }
+	static { RuntimeMetaData.checkVersion("4.8", RuntimeMetaData.VERSION); }
 
 	protected static final DFA[] _decisionToDFA;
 	protected static final PredictionContextCache _sharedContextCache =
